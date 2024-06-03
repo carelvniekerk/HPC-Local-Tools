521d5c0d84eb6cfaec302a1da5e8c6efbbcb209a
Carel van Niekerk <cvanniekerk@hhu.de>
2024-06-03 16:34:01 +0200

    chore: Add alias for syncing local huggingface models with HPC

--- tools.sh
+++ tools.sh
@@ -6,3 +6,6 @@ alias hpc_status="ssh Hilbert -t \"bash -ci 'base && home && qs'\""
 alias hpc_interactive_setup="ssh Hilbert -t \"bash -ci 'base && home && qi-setup'\""
 alias hpc_int="ssh Hilbert -t \"bash -ci 'base && home && qi'\""
 alias hpc_job_log="ssh Hilbert -t \"bash -ci 'base && home && job_log'\""
+
+# Alias for syncing local huggingface models with HPC
+alias sync_hf="rsync -avz --delete --progress /Users/carel17/.cache/huggingface Hilbert-Storage:/gpfs/project/niekerk/models/"
