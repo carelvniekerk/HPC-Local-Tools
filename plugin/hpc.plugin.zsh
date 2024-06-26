4d7d5d9de10d01824ebed370dffd1dfbcb0dba4a
Carel van Niekerk <cvanniekerk@hhu.de>
2024-06-26 15:21:54 +0200

    chore: Update HPC command aliases and add shorthand aliases for better readability and clarity

--- plugin/hpc.plugin.zsh
+++ plugin/hpc.plugin.zsh
@@ -3,3 +3,13 @@ fpath=($HOME/.oh-my-zsh/plugins/hpc_tools $fpath)
 # Configure HPC Tools
 export HPC_TOOLS_DIR="${HOME}/.hpc_tools"
 export PATH="${HPC_TOOLS_DIR}:${PATH}"
+
+# Shorthand Aliases
+alias hrun="hpc run"
+alias hs="hpc status"
+alias hqi="hpc qi"
+alias hcl="hpc log"
+alias hshf="hpc sync_hf"
+alias hslogs="hpc sync_job_logs"
+alias hlgn="hpc ssh"
+alias hcmd="hpc cmd"
