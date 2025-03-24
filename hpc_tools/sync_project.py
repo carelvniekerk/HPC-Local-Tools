d0a36eb536894cd753bf1dae8bb58ea408425650
Carel van Niekerk <vniekerk.carel@gmail.com>
2025-03-24 17:15:10 +0100

    Enhance sync_project function to support dynamic HPC system selection and improve remote storage node retrieval

--- hpc_tools/sync_project.py
+++ hpc_tools/sync_project.py
@@ -20,14 +20,26 @@
 # limitations under the License."
 
 import subprocess
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 from pathlib import Path
 
-from hpc_tools.constants import REMOTE_BASE, STORAGE_NODE
+from hpc_tools.constants import HPCSystem, NodeType, get_node, get_username
 
 
 def sync_project() -> None:
     """Sync the project with the remote storage node."""
-    local_dir = Path.cwd()
+    parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        "--hpc_name",
+        help="HPC System",
+        default=HPCSystem.HILBERT,
+        type=HPCSystem,
+    )
+    hpc_system: HPCSystem = parser.parse_args().hpc_name
+    storage_node: str = get_node(hpc_system, NodeType.STORAGE)
+    _, remote_base = get_username(hpc_system)
+
+    local_dir: Path = Path.cwd()
 
     # Check if there is a exclude file in the local directory
     exclude_file = local_dir / ".rsync-exclude"
@@ -42,17 +54,17 @@ def sync_project() -> None:
         exclude_args += ["--exclude-from", str(exclude_file)]
 
     # Sync local directory with remote directory, excluding .venv
-    print("Synchronizing files...")
     rsync_command = [
         "rsync",
         "-avz",
         "--delete",
         *exclude_args,
         str(local_dir),
-        f"{STORAGE_NODE}:{REMOTE_BASE}/",
+        f"{storage_node}:{remote_base}/",
     ]
 
     try:
+        print(rsync_command)
         subprocess.run(rsync_command, check=True)  # noqa: S603
     except subprocess.CalledProcessError as err:
         msg = f"Error during rsync: {err}"
